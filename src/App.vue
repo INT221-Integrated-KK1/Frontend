c609646cbceac0816f48525fbc7cc5799c7c947f
Pheeraphat47 <dpheeraphat2547@gmail.com>
2024-04-21 20:50:42 +0700

    fix project name

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <template>
 
   <header>
-    <h1 class="text-3xl text-center font-bold pt-5">IT Bangmod Kanban Board</h1>
+    <h1 class="text-3xl text-center font-bold pt-5">IT Bangmod Kradan Kanban</h1>
   </header>
 
 </template>
