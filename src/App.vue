1ebee7132bce1bfef1fab89088c0e4ad830fcc20
findmelily <pinny.pinngam@gmail.com>
2024-04-25 15:52:24 +0700

    update TaskDetail

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script setup>
+import { ref } from 'vue';
+import TaskDetail from './components/TaskDetail.vue';
+const showModal = ref(false);
+const closeModal = () => {
+  showModal.value = false;
+}
 
 </script>
 
@@ -9,42 +15,49 @@
   </header>
 
   <div class="overflow-x-auto mt-10">
-  <table class="table">
-    <!-- head -->
-    <thead>
-      <tr class="text-xl text-black">
-        <th></th>
-        <th>Title</th>
-        <th>Assignees</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- row 1 -->
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr class="text-xl text-black">
+          <th></th>
+          <th>Title</th>
+          <th>Assignees</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr>
+          <th>1</th>
+          <a @click="showModal = true">
+            <td>Cy Ganderton</td>
+          </a>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+        </tr>
+        <!-- row 2 -->
+        <tr>
+          <th>2</th>
+          <a @click="showModal = true">
+            <td>Hart Hagerty</td>
+          </a>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+        <!-- row 3 -->
+        <tr>
+          <th>3</th>
+          <a @click="showModal = true">
+            <td>Brice Swyre</td>
+          </a>
+          <td>Tax Accountant</td>
+          <td>Red</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 
+  <TaskDetail @closeModal="closeModal" :showModal="showModal" />
 </template>
 
 <style scoped></style>
--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,17 +1,17 @@
 <script setup>
 //demo modal
 import { ref } from 'vue';
-const showModal = ref(false);
+const { showModal } = defineProps(['showModal']);
+defineEmits(['closeModal']);
 
 //demo time to show
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const currentDate = new Date().toLocaleString();
-
+const currentDate = new Date().toLocaleString("En-US", timezone);
+const description = ref('');
 </script>
- 
+
 <template>
     <!-- demo button modal -->
-    <div class="btn" @click="showModal = true"> detail demo </div>
     <div v-if="showModal" class="text-black fixed z-10 inset-0 overflow-y-auto">
         <div class="flex items-center justify-center min-h-screen bg-black/[.05]">
             <div class="bg-white w-1/2 p-6 rounded shadow-lg grid grid-cols-3 gap-3">
@@ -23,8 +23,9 @@ const currentDate = new Date().toLocaleString();
                 <hr class="col-start-1 col-span-3">
                 <div class="col-start-1 col-span-2">
                     <h1 class="font-bold">Description : </h1>
-                    <textarea rows="10" placeholder="description"
-                        class="p-2 border-solid border-2 border-grey w-full"></textarea>
+                    <textarea rows="10" placeholder="No description provided"
+                        class="p-2 border-solid border-2 border-grey w-full"
+                        :class="description === '' ? 'italic text-gray-500' : 'not-italic text-black'">{{ description }}</textarea>
                 </div>
                 <div class="col-start-3 col-span-1 h-auto">
                     <h1 class="font-bold">Assignees : </h1>
@@ -42,13 +43,12 @@ const currentDate = new Date().toLocaleString();
                     <h1 class="font-bold">UpdatedOn : {{ currentDate }}</h1>
                 </div>
                 <div class="flex justify-end mt-4 col-start-3">
-                    <button @click="showModal = false" class="bg-red-500 text-white px-4 py-2 rounded">Close</button>
+                    <button @click="$emit('closeModal', false)"
+                        class="bg-red-500 hover:bg-red-700 text-white btn">Close</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
- 
-<style scoped>
 
-</style>
+<style scoped></style>
