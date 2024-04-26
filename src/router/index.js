40815e77667c7aa5b8b757b59569cc594e754e98
findmelily <pinny.pinngam@gmail.com>
2024-04-26 18:25:49 +0700

    edit conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import App from '@/App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import App from "@/App.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: "/",
+      redirect: "/task", // Redirect to '/task' when accessing the root path
     },
     {
       path: "/task",
       name: "task",
-      component: App
-    }
-  ]
-})
+      component: App,
+    },
+  ],
+});
 
-export default router
+export default router;
