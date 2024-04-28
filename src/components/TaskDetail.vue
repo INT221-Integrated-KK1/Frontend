600c1f719be4fd019a2fcaa73ca0f34a73736a5c
findmelily <pinny.pinngam@gmail.com>
2024-04-28 14:02:42 +0700

    all test pass without time

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -53,6 +53,7 @@ const formatToLocalTime = (dateTimeString) => {
   const localTime = new Date(dateTime - timezoneOffset)
   //return localTime.toLocaleString()
   const localDate = new Date(dateTime.getTime() - timezoneOffset);
+  console.log(localDate);
   return localDate.toLocaleString('en-GB', {
     day: '2-digit',
     month: '2-digit',
@@ -70,26 +71,26 @@ const formatToLocalTime = (dateTimeString) => {
   <div class="text-black fixed z-10 inset-0 overflow-y-auto">
     <div class="flex items-center justify-center min-h-screen bg-black/[.05]">
       <div class="bg-white w-1/2 p-6 rounded shadow-lg grid grid-cols-3 gap-3">
-        <div class="col-start-1 col-span-3">
+        <div class=" col-start-1 col-span-3">
           <h1 class="font-bold">Title :</h1>
-          <input class="itbkk-title p-2 border-solid border-2 border-grey w-full mb-3" v-model="taskTitle" />
+          <div class="itbkk-title p-2 border-solid border-2 border-grey w-full mb-3" v-text="taskTitle"></div>
         </div>
         <hr class="col-start-1 col-span-3" />
         <div class="col-start-1 col-span-2">
           <h1 class="font-bold">Description :</h1>
-          <textarea
-            class="itbkk-description placeholder:italic placeholder:text-slate-400 p-2 border-solid border-2 border-grey w-full"
-            rows="10" :class="taskDescription === null ? EmptyStyle : ''" :value="taskDescription === null ? EmptyDescriptionText : taskDescription
-            ">
-          </textarea>
+          <div
+            class="itbkk-description placeholder:italic placeholder:text-slate-400 p-2 border-solid border-2 border-grey w-full h-[14rem] break-words"
+            :class="taskDescription === null ? EmptyStyle : ''">
+            {{ taskDescription === null ? EmptyDescriptionText : taskDescription }}
+          </div>
         </div>
         <div class="col-start-3 col-span-1">
           <h1 class="font-bold">Assignees :</h1>
-          <textarea rows="2"
-            class="placeholder:italic placeholder:text-slate-400 p-2 border-solid border-2 border-grey w-full itbkk-assignees"
-            :class="taskAssignees === null ? EmptyStyle : ''"
-            :value="taskAssignees === null ? EmptyAssigneeText : taskAssignees">
-          </textarea>
+          <div
+            class="itbkk-assignees placeholder:italic placeholder:text-slate-400 p-2 border-solid border-2 border-grey w-full  break-words"
+            :class="taskAssignees === null ? EmptyStyle : ''">
+            {{ taskAssignees === null ? EmptyAssigneeText : taskAssignees }}
+          </div>
           <h1 class="font-bold">Status :</h1>
           <select class="p-2 border-solid border-2 border-grey w-full mb-10 itbkk-status" v-model="taskStatus">
             <option value="No Status">No Status</option>
@@ -103,7 +104,7 @@ const formatToLocalTime = (dateTimeString) => {
         </div>
         <div class="flex justify-end mt-4 col-start-3">
           <RouterLink :to="{ name: 'task' }">
-            <button @click="$emit('closeModal', false)" class="btn bg-red-500 hover:bg-red-700 text-white btn">
+            <button class="btn bg-red-500 hover:bg-red-700 text-white">
               Close
             </button>
           </RouterLink>
@@ -111,6 +112,7 @@ const formatToLocalTime = (dateTimeString) => {
       </div>
     </div>
   </div>
+
 </template>
 
 <style scoped></style>
