2ccae3b96a6a57e4697505fe03b7515ef097ff08
findmelily <pinny.pinngam@gmail.com>
2024-09-02 20:16:57 +0700

    uninstall unused packages

--- src/router/index.js
+++ src/router/index.js
@@ -77,9 +77,9 @@ const router = createRouter({
 });
 
 
-router.beforeEach((to, from, next) => {
-  if (to.name !== "login" && !localStorage.getItem('token')) next({ name: "login" });
-  else next();
-});
+// router.beforeEach((to, from, next) => {
+//   if (to.name !== "login" && !localStorage.getItem('token')) next({ name: "login" });
+//   else next();
+// });
 
 export default router;
